9d3fbc84c3a5b54919f30a85271bceb6033d09bc
Nawal <nawalx197@gmail.com>
2024-04-21 17:42:02 +0545

    Refactor Flask application to improve code readability and maintainability

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-from flask import Flask,render_template,request,url_for
-app=Flask(__name__)
+"""
+This module runs a Flask application.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    """
+    This function renders the home page.
+    """
+    return render_template("index.html")
+
 
 app.run(debug=True)
